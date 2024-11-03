3831e68bf7a09949034947bb77292e17f54790c6
shreyya18 <shreyapoojary251@gmail.com>
2024-11-03 22:07:11 +0530

    commented

--- javascript/to do app/todo.js
+++ javascript/to do app/todo.js
@@ -1,60 +1,61 @@
-const taskForm=document.getElementById("task-form");
-const openTaskFormBtn=document.getElementById("open-task-form-btn");
-const closeTaskFormBtn=document.getElementById("close-task-form-btn");
-const titleInput=document.getElementById("title-input");
-const dateInput=document.getElementById("date-input");
-const descriptionInput=document.getElementById("description-input");
-const confirmCloseDialog=document.getElementById("confirm-close-dialog");
-const cancelBtn=document.getElementById("cancel-btn");
-const discardBtn=document.getElementById("discard-btn");
-const addOrUpdateTaskBtn=document.getElementById("add-or-update-task-btn");
-const taskContainer=document.getElementById("tasks-container");
-function reset(){
-    titleInput.value=" ";
-}
-openTaskFormBtn.addEventListener("click", ()=>{
+// const taskForm=document.getElementById("task-form");
+// const openTaskFormBtn=document.getElementById("open-task-form-btn");
+// const closeTaskFormBtn=document.getElementById("close-task-form-btn");
+// const titleInput=document.getElementById("title-input");
+// const dateInput=document.getElementById("date-input");
+// const descriptionInput=document.getElementById("description-input");
+// const confirmCloseDialog=document.getElementById("confirm-close-dialog");
+// const cancelBtn=document.getElementById("cancel-btn");
+// const discardBtn=document.getElementById("discard-btn");
+// const addOrUpdateTaskBtn=document.getElementById("add-or-update-task-btn");
+// const taskContainer=document.getElementById("tasks-container");
+// function reset(){
+//     titleInput.value=" ";
+// }
+// openTaskFormBtn.addEventListener("click", ()=>{
 
-    taskForm.classList.toggle("hidden");
-}
-);
-cancelBtn.addEventListener("click", ()=>{
+//     taskForm.classList.toggle("hidden");
+// }
+// );
+// cancelBtn.addEventListener("click", ()=>{
 
-    confirmCloseDialog.close();
-}
-);
-closeTaskFormBtn.addEventListener("click",()=>{
+//     confirmCloseDialog.close();
+// }
+// );
+// closeTaskFormBtn.addEventListener("click",()=>{
 
-    confirmCloseDialog.showModal();
-}
-);
+//     confirmCloseDialog.showModal();
+// }
+// );
 
-discardBtn.addEventListener("click",()=>{
+// discardBtn.addEventListener("click",()=>{
 
-    taskForm.classList.toggle("hidden");
-    confirmCloseDialog.close();
-    reset();
-});
-const edit=(btnEL)=>{
+//     taskForm.classList.toggle("hidden");
+//     confirmCloseDialog.close();
+//     reset();
+// });
+// const edit=(btnEL)=>{
 
-}
+// }
 
-addOrUpdateTaskBtn.addEventListener("click", () => {
+// addOrUpdateTaskBtn.addEventListener("click", () => {
 
-    taskContainer.innerHTML += `
-    <div class="task">
-        <p><strong>Title: ${titleInput.value}</strong></p>
-        <p><strong>Date: ${dateInput.value}</strong></p>
-        <p><strong>Description: ${descriptionInput.value}</strong></p>
-        <button class="btn" type="button" onclick="edit(this)">edit</button>
-        <button class="btn" type="button" onclick="delete(this)">delete</button>
-    </div>`;
-    ; titleInput.value = ''; 
-    dateInput.value = ''; 
-    descriptionInput.value = ''; 
-    taskForm.classList.toggle("hidden");
+//     taskContainer.innerHTML += `
+//     <div class="task">
+//         <p><strong>Title: ${titleInput.value}</strong></p>
+//         <p><strong>Date: ${dateInput.value}</strong></p>
+//         <p><strong>Description: ${descriptionInput.value}</strong></p>
+//         <button class="btn" type="button" onclick="edit(this)">edit</button>
+//         <button class="btn" type="button" onclick="delete(this)">delete</button>
+//     </div>`;
+//     ; titleInput.value = ''; 
+//     dateInput.value = ''; 
+//     descriptionInput.value = ''; 
+//     taskForm.classList.toggle("hidden");
     
-});
+// });
 
-const delete=(btnEL)=>{
-    btnEL.title
-}
+// const delete=(btnEL)=>{
+//     btnEL.title
+// }
+
